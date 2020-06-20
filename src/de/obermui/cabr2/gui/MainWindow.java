7797f8dd7a97466fdcb7d0106d3a5e792390d477
6543 <6543@obermui.de>
2020-06-21 00:46:17 +0200

    Add Version Info (#53)
    * imprufe infoBox
    
    * add Menu + option to Display Version

--- src/de/obermui/cabr2/gui/MainWindow.java
+++ src/de/obermui/cabr2/gui/MainWindow.java
@@ -1,6 +1,7 @@
 package de.obermui.cabr2.gui;
 
 import de.obermui.cabr2.intern.Beryllium;
+import de.obermui.cabr2.intern.Const;
 import de.obermui.cabr2.models.SafetyDataSheet;
 
 import javax.swing.*;
@@ -14,6 +15,10 @@ public class MainWindow implements ActionListener {
 	private JButton bt_new;
 	private JButton bt_exit;
 	private JButton bt_importBe;
+
+	private JMenuBar mb_menu;
+	private JMenuItem mi_about_version;
+
 	private JPanel panel1;
 	private JFrame fm_main;
 	private ctx Ctx;
@@ -37,6 +42,20 @@ public class MainWindow implements ActionListener {
 		bt_importBe.addActionListener(this);
 		bt_new.addActionListener(this);
 
+		//add a Menu
+		mb_menu = new JMenuBar();
+
+		// create & init menu & menu items
+		JMenu m_about = new JMenu("About");
+		mi_about_version = new JMenuItem("Version");
+		mi_about_version.addActionListener(this);
+		m_about.add(mi_about_version);
+
+		// put menu things together
+		fm_main.setJMenuBar(mb_menu);
+		mb_menu.add(Box.createHorizontalGlue());
+		mb_menu.add(m_about);
+
 		Ctx.mainWindow = fm_main;
 		fm_main.setVisible(true);
 	}
@@ -57,6 +76,10 @@ public class MainWindow implements ActionListener {
 			}
 		} else if (e.getSource() == this.bt_importBe) {
 			importBe();
+		} else if (e.getSource() == this.mi_about_version) {
+			Dialogs.infoBox(this.fm_main, "Version: " + AppVersion, "Version");
+		} else {
+			System.out.println(e.getSource().toString());
 		}
 	}
 
